850a4c63c821ab043fb91d7ce10ba90e0b1262df
Lucas Pei <lucas.pei23@ycdsbk12.ca>
2021-02-09 14:42:01 +0000

    added rounding command

--- windchill.py
+++ windchill.py
@@ -17,4 +17,4 @@ wind_speed = float(input("What is the wind speed in km/h?: "))
 windchill = 13.12 + (.6215*temperature) - (11.37*wind_speed ** 0.16) + (.3965 * temperature*wind_speed ** 0.16)
 
 #print windchill
-print ("The temperature with windchill factored in is: " +str(windchill))
+print ("The temperature with windchill factored in is: " +str(round(windchill, 2)))
